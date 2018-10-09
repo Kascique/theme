b24a654f5ec1adc276522a4997a66339f8c3f926
Kascique Lowmans <kascique@themastercoder.com>
2018-10-09 12:14:56 -0400

    Upload JS files

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,36 @@
-#
+//open menu
+$("#menuBtn").click(function(){
+	$('#menu').css("width", "300px");
+});
+//close menu
+$("#menuClose").click(function(){
+	$('#menu').css("width", "0px");
+});
+
+  //Change Launch time
+  var  lunchTime = "2017/12/10";
+  
+  $("#days")
+  .countdown(lunchTime, function(event) {
+    $(this).text(
+      event.strftime('%D')
+    );
+  });
+  $("#hours")
+  .countdown(lunchTime, function(event) {
+    $(this).text(
+      event.strftime('%H')
+    );
+  });
+  $("#minutes")
+  .countdown(lunchTime, function(event) {
+    $(this).text(
+      event.strftime('%M')
+    );
+  });
+  $("#seconds")
+  .countdown(lunchTime, function(event) {
+    $(this).text(
+      event.strftime('%S')
+    );
+  });
